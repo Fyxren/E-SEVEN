510fd9024a7089e7b16252e8bde80cab76bf048c
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-07-28 22:34:09 +0200

    `Add` Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
-	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 </script>
 
 <div class="px-24">
 	<Navbar />
 	<slot />
+	<Footer />
 </div>
