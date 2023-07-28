4057b36306771d9be996ef0d7b74432f4b0d9a7a
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-07-28 21:12:59 +0200

    `Add` members section

---
+++ src/lib/components/MemberCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	type IMember = {
+		name: string;
+		description: string;
+		image: string;
+	};
+	export let member: IMember;
+</script>
+
+<div>
+	<img
+		src={member.image}
+		alt={`image ${member.name}`}
+		class="object-contain duration-300 rounded-lg shadow-xl saturate-50 hover:saturate-100"
+	/>
+	<h3 class="mt-2 text-4xl font-thin tracking-wide">{member.name}</h3>
+	<p>{member.description}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import MemberCard from '$lib/components/MemberCard.svelte';
 	import RT from '$lib/components/RegionText.svelte';
+	import members from '../data/members';
 </script>
 
-<main class="mt-24">
+<main class="my-24 space-y-20">
+	<!-- Header -->
 	<div class="grid grid-cols-2 gap-20">
 		<iframe
 			src="https://www.youtube-nocookie.com/embed/681juG4vjkI?controls=0"
@@ -11,12 +14,12 @@
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 			allowfullscreen
 			color="#596655"
-			class="rounded-lg w-full h-max aspect-video"
+			class="w-full rounded-lg h-max aspect-video"
 		/>
 		<div class="flex flex-col justify-around">
 			<div>
 				<h1 class="text-6xl font-thin">Wij zijn <span class="font-normal">E-SEVEN</span></h1>
-				<p class="text-2xl mt-2">Drie vrienden die al jaren muziek maken.</p>
+				<p class="mt-2 text-2xl">Drie vrienden die al jaren muziek maken.</p>
 				<div class="mt-5 space-y-2">
 					<p>
 						Wij spelen graag op bruiloften of andere evenementen! Ons repetoire is gevarieerd: van
@@ -33,5 +36,12 @@
 			</div>
 			<a href="/contact" class="btn btn-primary btn-wide">Contact</a>
 		</div>
+	</div>
+
+	<!-- Members -->
+	<div class="grid grid-cols-3 gap-32">
+		{#each members as x}
+			<MemberCard member={x} />
+		{/each}
 	</div>
 </main>
