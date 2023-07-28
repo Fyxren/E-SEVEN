e0b7bf81808d19833064900bb5206d07cdeb2fea
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-07-28 22:45:27 +0200

    `Add` scufferd error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="space-y-5 text-center py-28">
+	<p class="text-6xl font-bold tracking-widest text-error">{$page.status}</p>
+	<h1 class="font-bold tracking-wider text-8xl">{$page?.error?.message}</h1>
+</div>
