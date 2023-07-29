a66441ddf8d4a2d287788a062789b60ffc8fb82c
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-07-29 16:57:40 +0200

    `Add - Social` mail

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -38,6 +38,13 @@
 					/>
 				</svg>
 			</a>
+			<a target="_blank" href="mailto:esevenband@gmail.com">
+				<svg class="duration-300 fill-current hover:fill-primary" width="40" height="40" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M4 20q-.825 0-1.413-.588T2 18V6q0-.825.588-1.413T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.588 1.413T20 20H4Zm8-7L4 8v10h16V8l-8 5Zm0-2l8-5H4l8 5ZM4 8V6v12V8Z"
+					/>
+				</svg>
+			</a>
 		</div>
 	</div>
 </footer>
