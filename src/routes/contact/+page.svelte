715d090288f1d02d3ab2cde748aef115eaedbade
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-07-30 21:35:57 +0200

    `Add` contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<main class="my-10">
+	<h1 class="text-3xl font-thin text-center md:text-5xl">Contact</h1>
+	<p class="mt-2 text-center md:mt-10 md:text-lg">
+		Voor meer informatie zit je hier op de jusite plek, kies de gewenste methode voor contact:
+	</p>
+	<div class="flex flex-col justify-center gap-5 py-10 pb-10 md:pb-20 md:flex-row md:gap-10">
+		<a class="self-center btn btn-wide btn-primary" href="mailto:esevenband@gmail.com">
+			<svg
+				class="duration-300 fill-current"
+				width="32"
+				height="32"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M4 20q-.825 0-1.413-.588T2 18V6q0-.825.588-1.413T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.588 1.413T20 20H4Zm8-7L4 8v10h16V8l-8 5Zm0-2l8-5H4l8 5ZM4 8V6v12V8Z"
+				/>
+			</svg>
+			Mail
+		</a>
+		<a class="self-center btn btn-wide btn-outline btn-secondary" href="https://instagram.com/esevenmusic">
+			<svg
+				class="duration-300 fill-current"
+				width="32"
+				height="32"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248a4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008a3.004 3.004 0 0 1 0 6.008z"
+				/>
+				<circle cx="16.806" cy="7.207" r="1.078" />
+				<path
+					d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42a4.6 4.6 0 0 0-2.633 2.632a6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71c0 2.442 0 2.753.056 3.71c.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632a6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419a4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186c.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688a2.987 2.987 0 0 1-1.712 1.711a4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055c-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311a2.985 2.985 0 0 1-1.719-1.711a5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654c0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311a2.991 2.991 0 0 1 1.712 1.712a5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655c0 2.436 0 2.698-.043 3.654h-.011z"
+				/>
+			</svg>
+			Instagram
+		</a>
+	</div>
+</main>
